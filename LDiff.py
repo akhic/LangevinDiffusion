fixing the gradient calculation for likelikihood, also added gradient descent algorithm

LDiff.py
@@ -5,7 +5,7 @@ from Fourierbasis import generate_fourier_basis_function
 def logistic(z):
     return 1 / (1 + np.exp(-z))
  
-num_terms = 100
+num_terms = 1000
 coefficients = np.array([1/(i//2+1)**3 for i in range(num_terms)]).reshape(num_terms,1)
 f = generate_fourier_basis_function(coefficients)
 # Generate synthetic data
@@ -14,8 +14,8 @@ f = generate_fourier_basis_function(coefficients)
 n = 1000
 alpha = 2
 period = 2 * np.pi
-np.random.seed(42)
-X = 10 * (np.random.rand(n, 1)- 0.5)
+# np.random.seed(42)
+X = 100 * (np.random.rand(n, 1)- 0.5)
 y = (logistic(f(X)) +  np.random.randn(n, 1)/50 > 0.5).astype(int)
 
 # y = (4 + 3 * X + np.random.randn(100, 1) > 5).astype(int)
@@ -32,10 +32,16 @@ def prior_log_density(theta, gradient = False):
 def log_likelihood(theta, X, y, gradient = False):
     ff = generate_fourier_basis_function(theta)
     if gradient:
-        p = len(theta)
+        p = len(theta)//2
         k = np.arange(1,p+1).reshape(1,p)/period
+        diff = (y - logistic(ff(X))).T
+        sine_block = np.sin(np.dot(X,k))
+        cos_block = np.cos(np.dot(X,k))
+        concat_block = np.zeros((n,2*p))
+        concat_block[:,::2] = sine_block
+        concat_block[:,1::2] = cos_block
         # px1 array
-        return np.dot((y - logistic(ff(X))).T, np.sin(np.dot(X,k))).T
+        return np.dot(diff[::2], concat_block).T
         
     log_like = np.dot(y.T, ff(X)) - np.log(1 + np.exp(ff(X))).sum()
     return log_like
@@ -65,14 +71,27 @@ def langevin_algorithm(num_samples, step_size, initial_theta, X, y):
 
     return np.array(samples)
 
+def gradient_descent(num_iterations, step_size, initial_theta, X, y):
+    theta_current = initial_theta
+    samples = [theta_current]
+    p = len(initial_theta)
+    for i in range(num_samples):
+        grad_ll = grad_log_likelihood(theta_current, X, y)
+
+        theta_proposed = theta_current + step_size * grad_ll
+        theta_current = theta_proposed
+        samples.append(theta_current)
+    
+    return np.array(samples)
+
 # Set hyperparameters
-num_samples = 20000
+num_samples = 10000
 step_size = 0.00001
 # initial_theta = np.zeros(10).reshape(10,1)  # Initial guess for coefficients
 initial_theta = np.array([1/(i//2+1)**3 for i in range(10)]).reshape(10,1)
 # Run Langevin algorithm
 samples = langevin_algorithm(num_samples, step_size, initial_theta, X, y)
-
+samples_gd = gradient_descent(num_samples, step_size, initial_theta, X, y)
 # Plot the trace and histogram of samples
 # plt.figure(figsize=(10, 5))
 # plt.subplot(2, 1, 1)
@@ -100,6 +119,6 @@ for i in range(10):
     plt.xlabel('Iteration')
     plt.ylabel(f'$\\theta_{i+1}$')
     plt.title(f'Sampling for $\\theta_{i+1}$')
-plt.savefig('PosteriorLangevin.png')
+# plt.savefig('PosteriorLangevin.png')
 plt.tight_layout()
 plt.show()